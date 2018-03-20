90b90d96fb66c50a51b61072f081aa59a98642f4
ambrosektal <joe.spann@gmail.com>
2018-03-19 21:52:41 -0400

    new threading that might work....

--- reconscan_threaded_dirb_only.py
+++ reconscan_threaded_dirb_only.py
@@ -20,26 +20,26 @@
 ## worth anything anyway :)
 ###############################################################################################################
 
-import subprocess
 import multiprocessing
-from multiprocessing import Process, Queue
-import os
-import time
+import subprocess
 import threading
+import time
 from queue import Queue
+
 q = Queue()
 print_lock = threading.Lock()
 iplist = []
 
+
 def multProc(targetin, scanip, port):
     jobs = []
-    p = multiprocessing.Process(target=targetin, args=(scanip,port))
+    p = multiprocessing.Process(target=targetin, args=(scanip, port))
     jobs.append(p)
     p.start()
     return
 
 
-#def dnsEnum(ip_address, port):
+# def dnsEnum(ip_address, port):
 #    print "INFO: Detected DNS on " + ip_address + ":" + port
 #    if port.strip() == "53":
 #       SCRIPT = "./dnsrecon.py %s" % (ip_address)# execute the python script         
@@ -48,88 +48,98 @@ def multProc(targetin, scanip, port):
 
 def httpEnum(ip_address, port):
     print "INFO: Detected http on " + ip_address + ":" + port
-    print "INFO: Performing nmap web script scan for " + ip_address + ":" + port    
-    HTTPSCAN = "nmap -sV -Pn -vv -p %s --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt -oN /root/scripts/recon/recon_enum/results/lab2018/%s_http.nmap %s" % (port, ip_address, ip_address)
+    print "INFO: Performing nmap web script scan for " + ip_address + ":" + port
+    HTTPSCAN = "nmap -sV -Pn -vv -p %s --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt -oN /root/scripts/recon/recon_enum/results/lab2018/%s_http.nmap %s" % (
+    port, ip_address, ip_address)
     results = subprocess.check_output(HTTPSCAN, shell=True)
-    DIRBUST = "./dirbust.py http://%s:%s %s" % (ip_address, port, ip_address) # execute the python script
+    DIRBUST = "./dirbust.py http://%s:%s %s" % (ip_address, port, ip_address)  # execute the python script
     subprocess.call(DIRBUST, shell=True)
-    #NIKTOSCAN = "nikto -host %s -p %s > %s._nikto" % (ip_address, port, ip_address)
+    # NIKTOSCAN = "nikto -host %s -p %s > %s._nikto" % (ip_address, port, ip_address)
     return
+
 
 def httpsEnum(ip_address, port):
     print "INFO: Detected https on " + ip_address + ":" + port
-    print "INFO: Performing nmap web script scan for " + ip_address + ":" + port    
-    HTTPSCANS = "nmap -sV -Pn -vv -p %s --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt -oX /root/scripts/recon/recon_enum/results/lab2018/%s_https.nmap %s" % (port, ip_address, ip_address)
+    print "INFO: Performing nmap web script scan for " + ip_address + ":" + port
+    HTTPSCANS = "nmap -sV -Pn -vv -p %s --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-methods,http-method-tamper,http-passwd,http-robots.txt -oX /root/scripts/recon/recon_enum/results/lab2018/%s_https.nmap %s" % (
+    port, ip_address, ip_address)
     results = subprocess.check_output(HTTPSCANS, shell=True)
-    DIRBUST = "./dirbust.py https://%s:%s %s" % (ip_address, port, ip_address) # execute the python script
+    DIRBUST = "./dirbust.py https://%s:%s %s" % (ip_address, port, ip_address)  # execute the python script
     subprocess.call(DIRBUST, shell=True)
-    #NIKTOSCAN = "nikto -host %s -p %s > %s._nikto" % (ip_address, port, ip_address)
+    # NIKTOSCAN = "nikto -host %s -p %s > %s._nikto" % (ip_address, port, ip_address)
     return
 
+
 def nmapScan(ip_address):
-   ip_address = ip_address.strip()
-   print "INFO: Running general TCP/UDP nmap scans for " + ip_address
-   serv_dict = {}
-   TCPSCAN = "nmap -vv -Pn -A -sC -sS -T 4 -p- -oN '/root/scripts/recon/recon_enum/results/lab2018/%s.nmap' -oX '/root/scripts/recon/recon_enum/results/lab2018/nmap/%s_nmap_scan_import.xml' %s"  % (ip_address, ip_address, ip_address)
-   UDPSCAN = "nmap -vv -Pn -A -sC -sU -T 4 --top-ports 200 -oN '/root/scripts/recon/recon_enum/results/lab2018/%sU.nmap' -oX '/root/scripts/recon/recon_enum/results/lab2018/nmap/%sU_nmap_scan_import.xml' %s" % (ip_address, ip_address, ip_address)
-   results = subprocess.check_output(TCPSCAN, shell=True)
-   udpresults = subprocess.check_output(UDPSCAN, shell=True)
-   lines = results.split("\n")
-   for line in lines:
-      ports = []
-      line = line.strip()
-      if ("tcp" in line) and ("open" in line) and not ("Discovered" in line):
-         while "  " in line: 
-            line = line.replace("  ", " ");
-         linesplit= line.split(" ")
-         service = linesplit[2] # grab the service name
-	 port = line.split(" ")[0] # grab the port/proto
-         if service in serv_dict:
-	    ports = serv_dict[service] # if the service is already in the dict, grab the port list
-	 
-         ports.append(port) 
-	 serv_dict[service] = ports # add service to the dictionary along with the associated port(2)
-   
-   # go through the service dictionary to call additional targeted enumeration functions 
-   for serv in serv_dict: 
-      ports = serv_dict[serv]	
-      if (serv == "http"):
- 	 for port in ports:
-	    port = port.split("/")[0]
-	    multProc(httpEnum, ip_address, port)
-      elif (serv == "ssl/http") or ("https" in serv):
-	 for port in ports:
-	    port = port.split("/")[0]
-	    multProc(httpsEnum, ip_address, port)
-      
-   print "INFO: TCP/UDP Nmap scans completed for " + ip_address 
-   return
+    ip_address = ip_address.strip()
+    print "INFO: Running general TCP/UDP nmap scans for " + ip_address
+    serv_dict = {}
+    TCPSCAN = "nmap -vv -Pn -A -sC -sS -T 4 -p- -oN '/root/scripts/recon/recon_enum/results/lab2018/%s.nmap' -oX '/root/scripts/recon/recon_enum/results/lab2018/nmap/%s_nmap_scan_import.xml' %s" % (
+    ip_address, ip_address, ip_address)
+    UDPSCAN = "nmap -vv -Pn -A -sC -sU -T 4 --top-ports 200 -oN '/root/scripts/recon/recon_enum/results/lab2018/%sU.nmap' -oX '/root/scripts/recon/recon_enum/results/lab2018/nmap/%sU_nmap_scan_import.xml' %s" % (
+    ip_address, ip_address, ip_address)
+    results = subprocess.check_output(TCPSCAN, shell=True)
+    udpresults = subprocess.check_output(UDPSCAN, shell=True)
+    lines = results.split("\n")
+    for line in lines:
+        ports = []
+        line = line.strip()
+        if ("tcp" in line) and ("open" in line) and not ("Discovered" in line):
+            while "  " in line:
+                line = line.replace("  ", " ");
+            linesplit = line.split(" ")
+            service = linesplit[2]  # grab the service name
+            port = line.split(" ")[0]  # grab the port/proto
+            if service in serv_dict:
+                ports = serv_dict[service]  # if the service is already in the dict, grab the port list
+
+            ports.append(port)
+            serv_dict[service] = ports  # add service to the dictionary along with the associated port(2)
+
+    # go through the service dictionary to call additional targeted enumeration functions
+    for serv in serv_dict:
+        ports = serv_dict[serv]
+        if (serv == "http"):
+            for port in ports:
+                port = port.split("/")[0]
+                multProc(httpEnum, ip_address, port)
+        elif (serv == "ssl/http") or ("https" in serv):
+            for port in ports:
+                port = port.split("/")[0]
+                multProc(httpsEnum, ip_address, port)
+
+    print "INFO: TCP/UDP Nmap scans completed for " + ip_address
+    return
+
 
 def target_file():
-     f = open('/root/scripts/recon/recon_enum/results/targets.txt', 'r') # CHANGE THIS!! grab the alive hosts from the discovery scan for enum
-     for scanip in f:
-         iplist.append(str.strip(scanip))
-     print(iplist)
-     f.close()
+    f = open('/root/scripts/recon/recon_enum/results/targets.txt',
+             'r')  # CHANGE THIS!! grab the alive hosts from the discovery scan for enum
+    for scanip in f:
+        iplist.append(str.strip(scanip))
+    f.close()
+
 
 def scanning_job():
     nmapScan(iplist[0])
-    print(iplist[0], "scanning_job")
+    # print(iplist[0], "scanning_job")
     iplist.remove(iplist[0])
     with print_lock:
-        print(threading.current_thread().name)#, worker) #prevents the print from printing until unlocked
+        print(threading.current_thread().name, worker)  # prevents the print from printing until unlocked
 
-def threader(): #Actually performing the threading operation
+
+def threader():  # Actually performing the threading operation
     while True:
-        worker = q.get() # getting the worker from the queue and putting the worker to work.
+        worker = q.get()  # getting the worker from the queue and putting the worker to work.
         scanning_job()
         q.task_done()
 
-def starting_jobs():
-   t = threading.Thread(target=threader)
-   t.daemon = True #will die when the main thread dies
-   t.start()
-   print(iplist[0], "starting_jobs")
+
+# def starting_jobs():
+#   t = threading.Thread(target=threader)
+#   t.daemon = True #will die when the main thread dies
+#   t.start()
+#   print(iplist[0], "starting_jobs")
 
 # grab the discover scan results and start scanning up hosts
 print "############################################################"
@@ -137,34 +147,31 @@ print "####                      RECON SCAN                    ####"
 print "####            A multi-process service scanner         ####"
 print "####        http, ftp, dns, ssh, snmp, smtp, ms-sql     ####"
 print "############################################################"
-if __name__=='__main__':
+if __name__ == '__main__':
     target_file()
-#    for x in range(254): #workers
-#    t = threading.Thread(target=threader)
-#    t.daemon = True #will die when the main thread dies
-#    t.start()
-    for x in range(254):
-        starting_jobs()
+
+    for x in range(20):
+        t = threading.Thread(target=threader)
+        t.daemon = True  # will die when the main thread dies
+        t.start()
 
     start = time.time()
 
-    for worker in range(20): ##20 instances of workers / how many total jobs to run.
-        q.put(worker) #putting worker to work
+    for worker in range(260):  ##20 instances of workers / how many total jobs to run.
+        q.put(worker)  # putting worker to work
 
-    q.join() #waits till thread terminates
+    q.join()  # waits till thread terminates
 
-    print("Entire job took: ",time.time()-start)
+    print("Entire job took: ", time.time() - start)
 
     print("")
 
+# scan(scanip):
 
 
-#scan(scanip):
+# buildIPlist()
 
-
-#buildIPlist()
-
-#while len(IPlist):
+# while len(IPlist):
 #    if Thread.enumerate().isAlive() < 3:
 #        t = threading.Thread(target=nmapScan, args=IPlist[0])
 #        t.daemon
@@ -172,18 +179,16 @@ if __name__=='__main__':
 #        IPlist.remove(IPlist[0])
 
 
+# scan(ip)
+# checkVulns(ip)
 
-
-#scan(ip)
-#checkVulns(ip)
-
-#[
+# [
 #    ['scan','192,168,1,1'],
 #    ['scan','192.168.1.2'],
 #    ['checkVulns','192.168.1.1']
-#]
-#list[0][0](
+# ]
+# list[0][0](
 
 
-#test = 'scan'
-#test('192.168.1.1') === scan('192.168.1.1')
+# test = 'scan'
+# test('192.168.1.1') === scan('192.168.1.1')
